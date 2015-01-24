c96c4bc04ae38a5d3a0accc9474035f058beef44
Stefan Woskowiak <swoskowiak@gmail.com>
2015-01-24 17:06:12 -0500

    A whole lotta updates, added player, lighting effects etc.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render});
 
 function preload() {
-
-  //  Tilemaps are split into two parts: The actual map data (usually stored in a CSV or JSON file)
-  //  and the tileset/s used to render the map.
-
-  //  Here we'll load the tilemap data. The first parameter is a unique key for the map data.
-
-  //  The second is a URL to the JSON file the map data is stored in. This is actually optional, you can pass the JSON object as the 3rd
-  //  parameter if you already have it loaded (maybe via a 3rd party source or pre-generated). In which case pass 'null' as the URL and
-  //  the JSON object as the 3rd parameter.
-
-  //  The final one tells Phaser the foramt of the map data, in this case it's a JSON file exported from the Tiled map editor.
-  //  This could be Phaser.Tilemap.CSV too.
-
+  // Load the tilemap json
   game.load.tilemap('map1', 'maps/map1.json', null, Phaser.Tilemap.TILED_JSON);
-
-  //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
-
+  // Load the tiles png itself
   game.load.image('tiles', 'assets/tiles.png');
+  // Load the guard sprite
+  game.load.spritesheet('guard_sprite', 'assets/guard.png', 128, 128);
 }
 
-var map;
+var GLOBALS = {
+  tile_width: 64,
+  tile_height: 64
+}
 
+// Game objects
+var level,player;
+
+// Controls
+var cursors;
+
+// Where almost all our initialization happens
 function create() {
+  // some pass through stuff
+  game.GLOBALS = GLOBALS;
+  // Start physics
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  // Something like this
+  game.time.desiredFps = 60;
   // Will give you the level object which contains level.one, level.two etc.
-  var level = LEVELS.build(game);
+  level = LEVELS.build(game);
+  // Builds out our "player"
+  player = PLAYER.build(game);
+  // Gives you the cursor key controls
+  game.GLOBALS.cursors = game.input.keyboard.createCursorKeys();
+  game.GLOBALS.spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  console.log(game.GLOBALS.spacebar);
+}
+
+// Update stuff
+function update() {
+  PLAYER.update(game);
+}
+
+function render() {
+
 }
